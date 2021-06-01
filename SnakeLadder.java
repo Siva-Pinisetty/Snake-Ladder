4ef2ce5a00ffd6f4a56534cc0d36e7cd9ddf82b8
balu-pinisetty <balupinisetty@gmail.com>
2021-06-01 16:28:24 +0530

    Creating a Dice number

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -1,9 +1,13 @@
 package snakeladder;
-
+import java.util.Random;
 public class SnakeLadder {
 public static void main(String[] args) {
 	System.out.println("Welcome to Snake & Ladder Simulator");
 	int position=0;
 	System.out.println("Player intitial position is "+position);
+	Random random = new Random();
+	int Dice = random.nextInt(6)+1;
+	//double Dice= Math.floor(Math.random()*10)%6+1;
+	System.out.println("Dice number is "+Dice);
 }
 }
