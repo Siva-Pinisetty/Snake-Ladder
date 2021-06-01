f78fbf33cd969242c45e07a74be9898ac9f89f4d
balu-pinisetty <balupinisetty@gmail.com>
2021-06-01 17:15:22 +0530

    Changing player postion acc. to dice

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -7,7 +7,27 @@ public static void main(String[] args) {
 	System.out.println("Player intitial position is "+position);
 	Random random = new Random();
 	int Dice = random.nextInt(6)+1;
-	//double Dice= Math.floor(Math.random()*10)%6+1;
 	System.out.println("Dice number is "+Dice);
+	int Option = random.nextInt(3);
+	//System.out.println("Dice number is "+Option);
+	if (Option == 0)
+	{
+		System.out.println("No play");
+	}
+	else if (Option == 1)
+	{
+		System.out.println("Ladder and add to position");
+		position=+Dice;
+	}
+	else
+	{
+		System.out.println("Snake and subtract from position");
+		position=-Dice;
+		if (position < 0)
+		{
+			position=0;
+		}
+	}
+	System.out.println("Final position of player is "+position);
 }
 }
