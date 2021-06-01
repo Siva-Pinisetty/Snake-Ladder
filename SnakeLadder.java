a2ca8fd09d6fe941cda4bacd3706ed606fe67f61
balu-pinisetty <balupinisetty@gmail.com>
2021-06-01 17:52:18 +0530

    repeating to reach 100

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -4,30 +4,32 @@ public class SnakeLadder {
 public static void main(String[] args) {
 	System.out.println("Welcome to Snake & Ladder Simulator");
 	int position=0;
-	System.out.println("Player intitial position is "+position);
 	Random random = new Random();
-	int Dice = random.nextInt(6)+1;
-	System.out.println("Dice number is "+Dice);
-	int Option = random.nextInt(3);
-	//System.out.println("Dice number is "+Option);
-	if (Option == 0)
+	while(position<=100)
 	{
-		System.out.println("No play");
-	}
-	else if (Option == 1)
-	{
-		System.out.println("Ladder and add to position");
-		position=+Dice;
-	}
-	else
-	{
-		System.out.println("Snake and subtract from position");
-		position=-Dice;
-		if (position < 0)
+		int Dice = random.nextInt(6)+1;
+		System.out.println("Dice number is "+Dice);
+		int Option = random.nextInt(3);
+		//System.out.println("Dice number is "+Option);
+		if (Option == 0)
 		{
-			position=0;
+			System.out.println("No play");
 		}
+		else if (Option == 1)
+		{
+			System.out.println("Ladder and add to position");
+			position+=Dice;
+		}
+		else
+		{
+		System.out.println("Snake and subtract from position");
+		position-=Dice;
+			if (position < 0)
+			{
+				position=0;
+			}
+		}
+		System.out.println("Position of player is "+position);
 	}
-	System.out.println("Final position of player is "+position);
 }
 }
