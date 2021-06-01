5e4a0c5796156fbc198e971e5465f9da629cc6e3
balu-pinisetty <balupinisetty@gmail.com>
2021-06-01 16:05:23 +0530

    Intialising the position

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -3,5 +3,7 @@ package snakeladder;
 public class SnakeLadder {
 public static void main(String[] args) {
 	System.out.println("Welcome to Snake & Ladder Simulator");
+	int position=0;
+	System.out.println("Player intitial position is "+position);
 }
 }
